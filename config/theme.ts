7e8531d6efb104652ea1b73757b9be0b10fabdf9
Samiur Rahman <samiurrahman.dev@gmail.com>
2021-07-15 16:21:13 +0600

    chore(theme): ad mui theme

---
+++ config/theme.ts
@@ -0,0 +1,18 @@
+import { createTheme } from '@material-ui/core/styles';
+
+// Create a theme instance.
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#DE6A0A',
+    },
+    secondary: {
+      main: '#0BDE7D',
+    },
+    background: {
+      default: '#fff',
+    },
+  },
+});
+
+export default theme;
